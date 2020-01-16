a074aab7ee841ee308cef9c311c6229b2ad1021f
张鑫 <zxin088@gmail.com>
2020-01-16 10:32:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Usage
+1. `npm install --save-dev eslint-config-ospoon`
+2. create a file named `.eslintrc` in your project:
+
+```js
+{
+  "extends": "ospoon"
+  // Your overrides...
+}
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+const rules = require('./rules');
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    extends: ['plugin:vue/recommended', 'eslint:recommended'],
+
+    rules: rules
+}
